1612829c185eb71a7b02147e4b996814f9f5cd9b
Anastasiia1225 <83357931+Anastasiia1225@users.noreply.github.com>
2021-05-23 16:42:45 +0300

    add logic

--- model/index.js
+++ model/index.js
@@ -1,15 +1,65 @@
-// const fs = require('fs/promises')
-// const contacts = require('./contacts.json')
+const fs = require('fs/promises')
+const path = require('path')
+const {v4 : uuid} = require('uuid')
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, 'contacts.json')
 
-const getContactById = async (contactId) => {}
+const readData = async () => {
+  const data = await fs.readFile(contactsPath, 'utf-8')
+  return JSON.parse(data)
+}
 
-const removeContact = async (contactId) => {}
+const listContacts = async () => {
+  return await readData()
+}
 
-const addContact = async (body) => {}
+const getContactById = async (contactId) => {
+  const data = await readData()
+  const result = data.find(({id}) => id.toString() === contactId)
+  return result
+}
 
-const updateContact = async (contactId, body) => {}
+const removeContact = async (contactId) => {
+  const data = await readData()
+ 
+  const removedContactPosition = data.indexOf(data.find(({id}) => id.toString() === contactId))
+
+  if(removedContactPosition !== -1){
+    const removedContact = data.splice(removedContactPosition, 1)
+    await fs.writeFile(contactsPath, JSON.stringify(data))
+
+    return removedContact
+  }
+
+  return null
+}
+
+const addContact = async (body) => {
+  const id = uuid()
+  const record ={
+    id,
+    ...body
+  }
+
+  const data = await readData()
+
+  data.push(record)
+  await fs.writeFile(contactsPath, JSON.stringify(data))
+
+  return record
+}
+
+const updateContact = async (contactId, body) => {
+  const data = await readData()
+  const result = data.find(({id}) => id.toString() === contactId)
+
+  if(result){
+    Object.assign(result, body)
+    await fs.writeFile(contactsPath, JSON.stringify(data))
+  }
+
+  return result
+}
 
 module.exports = {
   listContacts,
