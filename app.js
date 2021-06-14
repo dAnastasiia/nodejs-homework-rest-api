e274dc321895e82f3803c50bef25da882dfcaa12
Anastasiia1225 <83357931+Anastasiia1225@users.noreply.github.com>
2021-06-14 14:42:23 +0300

    add static

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
+const path= require('path')
+require('dotenv').config()
 
 const usersRouter = require('./routes/api/users/index')
 const contactsRouter = require('./routes/api/contacts/index')
@@ -12,6 +14,8 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
+
+app.use(express.static(path.join(__dirname, process.env.IMAGE_DIR)))
 
 app.use('/api/users', usersRouter)
 app.use('/api/contacts', contactsRouter)
